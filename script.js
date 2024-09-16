22cf0e7cca78a54a07a66c4545b0bdad8ffa87c8
HarshalAl02 <alaspureh@gmail.com>
2024-09-16 12:02:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const submitBtn = document.getElementById("submitBtn");
+
+    submitBtn.addEventListener("click", function() {
+        alert("Message sent! We will get back to you soon.");
+    });
+});
